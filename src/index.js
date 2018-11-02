a03fac907d2a9d7bfc2507cd7845d582aa4a58d6
rene <renegmed@yahoo.com>
2018-11-02 14:30:24 -0400

    refactored action, reducer and constants into one file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import store from './store'; 
 import * as FLAVORS from './constants/flavors';
 
-import { addProductToFreezer, updateTemperature } from './actions/freezer';
+import { actions } from './ducks/freezer';
 
 store.subscribe(() => console.log(store.getState()));  //listens to change of state and log the change
 
-store.dispatch(updateTemperature(-8));
+store.dispatch(actions.updateTemperature(-8));
  
-store.dispatch(addProductToFreezer(FLAVORS.VANILLA, 15));
+store.dispatch(actions.addProductToFreezer(FLAVORS.VANILLA, 15));
  
