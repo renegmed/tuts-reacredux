03af28278658e3f1f4225316699a9f34de9d4390
rene <renegmed@yahoo.com>
2018-11-02 12:12:39 -0400

    saving freezer temp, created action and reducer

---
+++ src/actions/freezer.js
@@ -0,0 +1,9 @@
+import { UPDATE_TEMPERATURE } from '../constants/freezer';
+
+
+export function updateTemperature(temperature) {
+    return {
+        type: UPDATE_TEMPERATURE,
+        payload: temperature,
+    };
+}
---
+++ src/reducers/freezer.js
@@ -0,0 +1,15 @@
+import { UPDATE_TEMPERATURE } from '../constants/freezer';
+
+const DEFAULT_STATE = {
+    temperature: null,
+}
+export default function freezerReducer(state = DEFAULT_STATE, action) {
+    switch (action.type) {
+        case UPDATE_TEMPERATURE: 
+            return{
+                ...state,
+                temperature: action.payload,
+            }
+    }
+    return state;
+}
