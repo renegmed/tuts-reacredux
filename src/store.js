f9c3a256451b6e5881e123a12b352916cf22bec7
rene <renegmed@yahoo.com>
2018-11-03 10:16:54 -0400

    code clean up, removed logger2 middleware

--- src/store.js
+++ src/store.js
@@ -2,11 +2,10 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { reducer as freezer} from './ducks/freezer';
 import { reducer as orders } from './ducks/orders';
 import logger from './middleware/logger';
-import logger2 from './middleware/logger2';
-// adding a dummy reducer foo
+
 const rootReducer = combineReducers({
     freezer,
     orders,
 });
 
-export default createStore(rootReducer, applyMiddleware(logger, logger2));
+export default createStore(rootReducer, applyMiddleware(logger));
