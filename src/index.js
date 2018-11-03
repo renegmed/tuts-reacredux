8f51e9b9a5fa175daabdb0b61dd3ec00fa170cfc
rene <renegmed@yahoo.com>
2018-11-03 10:14:56 -0400

    added middleware, called before hitting the store and store reducer

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import store from './store';
 import * as FLAVORS from './constants/flavors';
 
 import { actions } from './ducks/freezer';
-
-store.subscribe(() => console.log(store.getState()));  //listens to change of state and log the change
-
+ 
 store.dispatch(actions.updateTemperature(-8));
  
 store.dispatch(actions.addProductToFreezer(FLAVORS.VANILLA, 15));
--- src/store.js
+++ src/store.js
@@ -1,11 +1,12 @@
-import { createStore, combineReducers} from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { reducer as freezer} from './ducks/freezer';
-
-
+import { reducer as orders } from './ducks/orders';
+import logger from './middleware/logger';
+import logger2 from './middleware/logger2';
 // adding a dummy reducer foo
 const rootReducer = combineReducers({
     freezer,
-    foo: (state = {hello: 'world'}, action) => state,
+    orders,
 });
 
-export default createStore(rootReducer);
+export default createStore(rootReducer, applyMiddleware(logger, logger2));
