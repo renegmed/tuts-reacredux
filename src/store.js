7198f675faad1e86550ba1253af0657923653269
rene <renegmed@yahoo.com>
2018-11-03 21:43:49 -0400

    used redux devtools

--- src/store.js
+++ src/store.js
@@ -1,12 +1,15 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import { reducer as freezer} from './ducks/freezer';
 import { reducer as orders } from './ducks/orders';
-import logger from './middleware/logger';
-
+  
 const rootReducer = combineReducers({
     freezer,
     orders,
 });
 
-export default createStore(rootReducer, applyMiddleware(thunk, logger));
+export default createStore(rootReducer, composeWithDevTools(
+    applyMiddleware(thunk)
+));
