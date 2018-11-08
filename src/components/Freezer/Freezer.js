4a1a2897cfbb3447530c936e962908c71b4f5d09
rene <renegmed@yahoo.com>
2018-11-08 07:00:14 -0500

    filled freezer with flavors default state in ui Freezer.

--- src/components/Freezer/Freezer.js
+++ src/components/Freezer/Freezer.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import store from '../../store';
 
 import Panel from '../Panel/Panel';
 import FreezerFlavor from '../FreezerFlavor/FreezerFlavor';
 
 class Freezer extends Component {
+  state = {
+    flavors: store.getState().freezer.flavors,
+  };
+
   render() {
+    const flavors = Object.keys(this.state.flavors).map(flavorName => (
+      <FreezerFlavor key={flavorName} flavorName={flavorName} scoops={this.state.flavors[flavorName]} />
+    ));
+    console.log(this.state.flavors);
     return (
       <Panel title="Freezer (°0C)">
-        <FreezerFlavor flavorName="Vanilla" scoops={50} />
+      {flavors}
       </Panel>
     );
   }
