67f8d3a0db21ed6d3faf6ce6cf50249a31c6a845
rene <renegmed@yahoo.com>
2018-11-08 10:54:07 -0500

    added ability to add products to the freezer component.

--- src/components/Freezer/Freezer.js
+++ src/components/Freezer/Freezer.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import store from '../../store';
 import { actions } from '../../ducks/freezer';
+import * as FLAVORS from '../../constants/flavors';
 
 import Panel from '../Panel/Panel';
+import Button from '../Button/Button';
 import FreezerFlavor from '../FreezerFlavor/FreezerFlavor';
 
 class Freezer extends Component {
@@ -38,6 +40,14 @@ class Freezer extends Component {
     }
   };
 
+  handleClickAddProduct = () => {
+    const allAvailableFlavors = Object.keys(FLAVORS);
+    const flavorName = window.prompt(`Enter flavor name to restock. (Choose from: ${allAvailableFlavors.join(', ')})`);
+
+    if (FLAVORS[flavorName]) {        
+      this.handleClickRestock(flavorName);
+    }
+  }
   render() {
     const flavors = Object.keys(this.state.flavors).map(flavorName => (
       <FreezerFlavor 
@@ -51,6 +61,8 @@ class Freezer extends Component {
     // used templating technique `` for title
     return (
       <Panel title={`Freezer (°${this.state.temperature || 0}C)`}>
+      <Button label="Add product" onClick={this.handleClickAddProduct}/>
+      <br/>
       {flavors}
       </Panel>
     );
