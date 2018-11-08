d7d8b38501e6760905fa3f1fb96590f3e140ff4f
rene <renegmed@yahoo.com>
2018-11-08 10:05:31 -0500

    freezer component temperature subscribed to changing temperature. used templating technique

--- src/components/Freezer/Freezer.js
+++ src/components/Freezer/Freezer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import store from '../../store';
+import { actions } from '../../ducks/freezer';
 
 import Panel from '../Panel/Panel';
 import FreezerFlavor from '../FreezerFlavor/FreezerFlavor';
@@ -7,14 +8,22 @@ import FreezerFlavor from '../FreezerFlavor/FreezerFlavor';
 class Freezer extends Component {
   state = {
     flavors: store.getState().freezer.flavors,
+    temperature: store.getState().freezer.temperature,
   };
 
   componentDidMount() {
     this.unsubscribe = store.subscribe(() => {
       this.setState({
         flavors: store.getState().freezer.flavors,
+        temperature: store.getState().freezer.temperature,
       });
     });
+
+    setInterval(() => {
+      const randomTemperature = -Math.round(Math.random() * 10);
+      //console.log("random temperature:", randomTemperature);
+      store.dispatch(actions.updateTemperature(randomTemperature));
+    }, 2000);
   }
 
   componentWillUnmount() {
@@ -25,9 +34,12 @@ class Freezer extends Component {
     const flavors = Object.keys(this.state.flavors).map(flavorName => (
       <FreezerFlavor key={flavorName} flavorName={flavorName} scoops={this.state.flavors[flavorName]} />
     ));
-    console.log(this.state.flavors);
+    //console.log(this.state.flavors);
+    //console.log(this.state.temperature);
+
+    // used templating technique `` for title
     return (
-      <Panel title="Freezer (°0C)">
+      <Panel title={`Freezer (°${this.state.temperature || 0}C)`}>
       {flavors}
       </Panel>
     );
