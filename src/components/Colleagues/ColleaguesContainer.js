a8467e554ce5ec14bb30879b24155bd250a0759d
rene <renegmed@yahoo.com>
2018-11-09 10:48:51 -0500

    set ColleaguesContainer reac-redux bindings on action dispatching and mapping of properties

--- src/components/Colleagues/ColleaguesContainer.js
+++ src/components/Colleagues/ColleaguesContainer.js
@@ -6,13 +6,25 @@ import { actions as employeesActions } from '../../ducks/employees';
 import Colleagues from './Colleagues';
 
 class ColleaguesContainer extends Component {
+
+    componentDidMount() {
+        this.props.fetchEmployees();
+    }
+    
     render() {
         return (
             <Colleagues />
         );
     }
 }
- 
 
-export default connect()(ColleaguesContainer);
+const mapStateToProps = (state) => ({
+    employees: state.employees,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    fetchEmployees: () => dispatch(employeesActions.fetchEmployees()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ColleaguesContainer);
 
